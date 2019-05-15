322da7276dcc885d74c8af264fa060b127d4475f
Nowosad <tupiszakaczki@gmail.com>
2019-05-15 14:30:02 +0200

    simplify test

---
+++ R/test2.R
@@ -0,0 +1,50 @@
+library(tmap)
+library(raster)
+library(rgdal)
+
+T2 <- "data/t2.tif"
+T2 <- raster(T2)
+proj4 <- projection(T2)
+
+pol <- readOGR("data/POL_adm0.shp")
+pol <- spTransform(pol, proj4)
+
+input <- T2
+obj <- mask(input, pol)
+
+minval <- minValue(obj)
+maxval <- maxValue(obj)
+
+breaks <-seq(minval, maxval, (maxval - minval) / 70)
+
+tempcolores <- c("#f6c39f","#e3ac89","#cb9881","#b58575","#9c716e","#865c62","#704754",
+                "#57344a","#3f1f3f","#240d2b","#260225","#2e0331","#370938","#420a40",
+                "#431243","#481046","#571658","#5e185e","#5f1b60","#671e67","#6d2069",
+                "#853a85","#964299","#9a559d","#a665a3","#ae74a9","#b485b3","#ba93b9",
+                "#c6a5c5","#cbb4cb","#d3c1d2","#c3cad5","#b6b7c6","#9ca4b9","#8992b0",
+                "#5e689b","#5e699d","#48528f","#374182","#1d2e77","#0b1761","#162e74",
+                "#234080","#37578c","#456f9a","#5a88ab","#78b2c4","#9fdbdc","#b1f0ee",
+                "#83c9a7","#72c29a","#67b78c","#69ba8f","#61b080","#56a573","#4c9d64",
+                "#3a9152","#368a45","#2a7f39","#2b7234","#1d681c","#29741a","#44851e",
+                "#578c25","#759c2b","#84a935","#afbf3c","#d8d952","#d4d755","#efe362",
+                "#e9d04f","#e1b845","#d9a53f","#c68f3d","#cc8c38","#c27b31","#ba6323",
+                "#b74d22","#ac4e28","#9f2715","#7b1b11","#80110c","#741105","#6f0d07",
+                "#630c06","#5a0c0c","#540904","#4b0504","#400401","#3f0101","#2d0708",
+                "#442321","#583e3a","#6f5652","#866e6a","#9c8982","#b2a59c","#c8bcb1",
+                "#c9bdb1","#ddd5c9","#f5efe3","#f4efe3")
+
+tm_shape(obj) +
+  tm_raster(
+    title = "title"  ,
+    palette = tempcolores,
+    breaks = breaks,
+    interpolate = TRUE
+  ) 
+
+tm_shape(obj + 5) +
+  tm_raster(
+    title = "title"  ,
+    palette = tempcolores,
+    breaks = breaks,
+    interpolate = TRUE
+  ) 
